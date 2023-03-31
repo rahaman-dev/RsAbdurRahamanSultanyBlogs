33d4ba9dd0e036e8aaa4e3ea9477ab4420a74eb2
rs rahaman <jesminakter.rahaman@gmail.com>
2023-03-31 19:53:10 +0600

    edit blog title and blogs name etc

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [times, setTimes] = useState([]);
   const [title, setTitle] = useState([]);
   const [toasts, setToasts] = useState(false);
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <div className="Header">
-        <h1>Knowledge Cafe</h1>
+        <h1>Rs Abdur Rahaman Sultany Blogs</h1>
         <img
           src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
           alt=""
